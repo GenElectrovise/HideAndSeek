6679dd373dcbe28a3d962d7bf83f7f1b42057746
GenElectrovise <sanchez.pamelos@outlook.com>
2020-04-26 14:07:28 +0100

    Full incorporation of the GNU Public License

--- src/main/java/net/clicksminuteper/HideAndSeek/main/command/CmdDestroyGame.java
+++ src/main/java/net/clicksminuteper/HideAndSeek/main/command/CmdDestroyGame.java
@@ -1,3 +1,21 @@
+/*
+ * HideAndSeek -- A Hide and Seek plugin for Bukkit and Spigot
+    Copyright (C) 2020 GenElectrovise
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.clicksminuteper.HideAndSeek.main.command;
 
 import org.bukkit.command.Command;
@@ -5,7 +23,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import net.clicksminuteper.HideAndSeek.main.game.Game;
-import net.clicksminuteper.HideAndSeek.main.game.ThreeDCoordinate;
+import net.clicksminuteper.HideAndSeek.main.util.ThreeDCoordinate;
 
 /**
  * Destroys the Game at the given coordinates, or the nearest Game to those if a
